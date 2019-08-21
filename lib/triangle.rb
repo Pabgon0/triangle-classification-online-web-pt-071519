127edf06a1ee0d9b7e452235c6bfefae16a59239
Pablo Gonzalez <pabgon0@gmail.com>
2019-08-21 23:07:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
   def real_triangle
     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
       raise TriangleError
-    else (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+    else ((@side_1+@side_2) <= @side_3) || ((@side_1+@side_3) <= @side_2) || ((@side_2+@side_3) <= @side_1)
       raise TriangleError
     end
   end
