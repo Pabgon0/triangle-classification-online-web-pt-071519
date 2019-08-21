f14cb172a2726c5c3db14548fee3175f1b7e6e8f
Pablo Gonzalez <pabgon0@gmail.com>
2019-08-21 22:53:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,11 +14,11 @@ class Triangle
       raise TriangleError
     else
       if (@side_1 == @side_2) && (@side_2 == @side_3)
-        :equilateral
+        return ":equilateral"
       elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
-        :isosceles
+        return ":isosceles"
       elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
-        :scalene
+        return ":scalene"
       end
   end
   
